4c8855c419a233224c290390f0c6a1d2b25ab1c8
JD <65065236+D0mnik@users.noreply.github.com>
2020-10-18 23:11:10 +0200

    implement  filter

--- ActReport.ViewModel/EmployeeViewModel.cs
+++ ActReport.ViewModel/EmployeeViewModel.cs
@@ -11,9 +11,12 @@ namespace ActReport.ViewModel
     {
         private string _firstName;
         private string _lastName;
+        private string _filter;
         private Employee _selectedEmployee;
         private ObservableCollection<Employee> _employees;
         private ICommand _cmdSaveChanges;
+        private ICommand _cmdNewEmployee;
+        private string _filterEmployee = "";
 
         public string FirstName
         {
@@ -24,7 +27,6 @@ namespace ActReport.ViewModel
                 OnPropertyChanged(nameof(FirstName));
             }
         }
-
         public string LastName
         {
             get => _lastName;
@@ -32,6 +34,25 @@ namespace ActReport.ViewModel
             {
                 _lastName = value;
                 OnPropertyChanged(nameof(LastName));
+            }
+        }
+        public string Filter
+        {
+            get => _filter;
+
+            set
+            {
+                _filter = value;
+                LoadEmployees();
+            }
+        }
+        public string FilterEmployee
+        {
+            get => _filterEmployee;
+            set
+            {
+                _filterEmployee = value;
+                LoadEmployees();
             }
         }
         public Employee SelectedEmployee
@@ -58,7 +79,7 @@ namespace ActReport.ViewModel
         {
             get
             {
-                if(_cmdSaveChanges == null)
+                if (_cmdSaveChanges == null)
                 {
                     _cmdSaveChanges = new RelayCommand(
                         execute: _ =>
@@ -71,7 +92,7 @@ namespace ActReport.ViewModel
 
                             LoadEmployees();
                         },
-                        canExecute: _ => _selectedEmployee != null);
+                        canExecute: _ => _selectedEmployee != null && LastName.Length >= 3);
                 }
 
                 return _cmdSaveChanges;
@@ -81,20 +102,50 @@ namespace ActReport.ViewModel
                 _cmdSaveChanges = value;
             }
         }
-        
+        public ICommand CmdNewEmployee
+        {
+            get
+            {
+                if (_cmdNewEmployee == null)
+                {
+                    _cmdNewEmployee = new RelayCommand(
+                        execute: _ =>
+                        {
+                            using UnitOfWork uow = new UnitOfWork();
+
+                            Employee emp = new Employee
+                            {
+                                FirstName = _firstName,
+                                LastName = _lastName
+                            };
+
+                            uow.EmployeeRepository.Insert(emp);
+                            uow.Save();
+
+                            LoadEmployees();
+                        },
+                        canExecute: _ => _firstName?.Length > 1 && _lastName?.Length > 1);
+                }
+
+                return _cmdNewEmployee;
+            }
+            set
+            {
+                _cmdNewEmployee = value;
+            }
+        }
         public EmployeeViewModel()
         {
             LoadEmployees();
         }
-
         private void LoadEmployees()
         {
-            using(UnitOfWork uow = new UnitOfWork())
+            using (UnitOfWork uow = new UnitOfWork())
             {
                 var employees = uow.EmployeeRepository
                     .Get(
-                        orderBy:
-                            coll => coll.OrderBy(employees => employees.LastName))
+                        orderBy: coll => coll.OrderBy(employees => employees.LastName),
+                        filter: emp => emp.LastName.StartsWith(FilterEmployee))
                     .ToList();
 
                 Employees = new ObservableCollection<Employee>(employees);
